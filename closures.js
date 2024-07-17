7d9420d5f5e33158e859b1a527f6e6e0455c2401
Areeba Farhan1234 <areebakhan1454771@gmail.com>
2024-07-17 16:43:02 +0500

    first commit

---
+++ closures.js
@@ -0,0 +1,29 @@
+// Closures Function
+
+function createCounter() {
+  let count = 0;
+
+  return function() {
+    count += 1;
+    return count;
+  };
+}
+
+const counter = createCounter();
+
+console.log(counter()); // Output: 1
+console.log(counter()); // Output: 2
+console.log(counter()); // Output: 3
+
+function outerFun(a) {
+  console.log("Hello Areeba" + a)
+  let c = 3;
+  let b = 5;
+  return function () {
+    return a+b+c;
+  }
+
+}
+
+let store = outerFun(2);
+console.log(store());
