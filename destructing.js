2a705adcd6fad068b5c73f2427270f0bb261e4b6
Areeba Farhan1234 <areebakhan1454771@gmail.com>
2024-07-18 05:38:23 +0500

    commit change

---
+++ destructing.js
@@ -0,0 +1,30 @@
+// Destructing Function
+
+//Destructuring Arrays  
+
+
+// const numbers = ["Apple", "Mango", "Grape"];
+
+// const [first, second, third] = numbers;
+
+// console.log(first);  // Output: 1
+// console.log(second); // Output: 2
+// console.log(third);  // Output: 3
+
+
+
+//Destructuring Objects
+
+
+// const person = {
+//   name: 'John',
+//   age: 30,
+//   city: 'New York'
+// };
+
+// const { name, age, city } = person;
+
+// console.log(name); // Output: John
+// console.log(age);  // Output: 30
+// console.log(city); // Output: New York
+
---
+++ spread-RestOperator.js
@@ -0,0 +1,37 @@
+// Spread and Rest Operator Function
+
+// //Spread Operator 
+
+// const numbers = [1, 2, 3];
+
+// const moreNumbers = [...numbers, 4, 5, 6];
+
+// console.log(moreNumbers); // Output: [1, 2, 3, 4, 5, 6]
+
+// //Rest Operator 
+
+// function sum(...args) {
+//   return args.reduce((total, num) => total + num, 0);
+// }
+
+// console.log(sum(1, 2, 3));    // Output: 6
+// console.log(sum(4, 5, 6, 7)); // Output: 22
+
+
+// //Spread and Rest Operators
+
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+
+
+// const mergedArray = [...arr1, ...arr2];
+
+// console.log(mergedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+// function concatenateStrings(...strings) {
+//   return strings.join(' ');
+// }
+
+// console.log(concatenateStrings('Hello', 'world!'));      // Output: "Hello world!"
+// console.log(concatenateStrings('JavaScript', 'is', 'Interested Language!')); // Output: "JavaScript is fun!"
